a10cbd184eaee40124559a3d78e8d7f6967d851c
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2017-03-07 15:36:29 +0100

    Database ready

--- database/DB.js
+++ database/DB.js
@@ -33,7 +33,109 @@ Post.hasMany(Comment)
 Comment.belongsTo(Post)
 
 //Relation between user and comment.
+Comment.belongsTo(User)
 User.hasMany(Comment)
 
-db.sync( {force: true})
+///Demo data
+///This does work. The first part is mine.
+db.sync({force: true}).then( done => {
+	return Promise.all([
+		//demo data in user table
+		User.create({
+			firstname: 'Dhova',
+			lastname: 'Kin',
+			email: 'dragon@gmail.com',
+			password: 'werewolf',
+			username: 'DhovaKing'
+		}),
+		User.create({
+			firstname: 'Bruce',
+			lastname: 'Banner',
+			email: 'prof_Banner@hotmail.com',
+			password: 'Skaar#Betty',
+			username: 'NerdRage'
+		})
+	])
+///The 
+}).then( users => {
+	return Promise.all([
+		//Demo data belonging to the first user.
+		users[0].createPost({
+			title: "Learned new shout.",
+			post: "Wuld!!"
+		}).then(comment=> {
+			comment.createComment({
+				comment: 'Cool story bro.'
+			}).then(thecomment => {
+				thecomment.setUser(users[0])
+			})
+		}),
 
+		//Demo data belonging to the second user.
+		users[1].createPost({
+			title: "Angry.",
+			post: "Betty where"
+		}).then(comment=> {
+			comment.createComment({
+				comment: 'Ross sux.'
+			}).then(thecomment => {
+				thecomment.setUser(users[1])
+			})
+		})
+	])
+} )
+
+
+
+////This did not work///////////////////////////
+///I tried to acces 2 promise.all returns in one 
+///promise but it did not work.
+// //sync database and create demo data
+// db.sync( {force: true}).then( function() {
+// 	return Promise.all([
+
+// 		Promise.all([
+// 			//demo data in user table
+// 			User.create({
+// 				firstname: 'Dhova',
+// 				lastname: 'Kin',
+// 				email: 'dragon@gmail.com',
+// 				password: 'werewolf',
+// 				username: 'DhovaKing'
+// 			}),
+// 			User.create({
+// 				firstname: 'Bruce',
+// 				lastname: 'Banner',
+// 				email: 'prof_Banner@hotmail.com',
+// 				password: 'Skaar#Betty',
+// 				username: 'NerdRage'
+// 			})
+
+// 			//demo data in post table. Two per user. 
+// 		]),
+// 		Promise.all([ 
+// 			users[0].createPost({
+// 				title: "Learnd new shout.",
+// 				post: "Wuld!!"
+// 			}),
+
+// 			users[0].createPost({
+// 				title: "I got married.",
+// 				post: "It's Mjoll"
+// 			}),
+			
+// 			users[1].createPost({
+// 				title: "Angry.",
+// 				post: "Betty where?"
+// 			}),
+
+// 			users[1].createPost({
+// 				title: "Ross.",
+// 				post: "Please leave me alone."
+// 			})
+// 		])
+// 	])
+// }).then( doubleresult => {
+// 	console.log(doubleresult)
+// } )
+/////////////////////////////////////////////
