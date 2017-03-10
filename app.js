1b5ecf40d5022c49ed2664282848ac9f4174de3b
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2017-03-10 17:32:27 +0100

    setup for the logout, does not work.

--- app.js
+++ app.js
@@ -38,16 +38,27 @@ app.get('/', function (req,res) {
 
 app.post('/login', function(req,res){
 	//if (req.body.username == users.username && 
-	//	 req.body.password == users.password)
+	//	 req.body.password == users.password) { 
+	//				
+	//}
 
+	//Test req.body
 	console.log('Login post body is ', req.body)
 	//Test session.
 	console.log(req.session)
+
+	//Pass form username into req.session.activeUser
 	req.session.activeUser = req.body.username
+
+	//This will then render the home page after the username and
+	//the password are confirmed. 
 	res.render('home', {
 		username: req.session.activeUser
 	})
-	//else res.render('login')
+	//This is what happends if the login fails.
+	//else { res.render('login', {
+	//			loginfail: 'Username or password not found.' 
+	//}) }
 })
 
 
@@ -61,6 +72,15 @@ app.get('/register', function (req,res) {
 /////////The router/////////
 //Username is the username of the current loggedin user.
 app.use('/username', routers)
+
+//The logout
+// //THis does not yet work.///
+// //Destroy the session uppon logout.
+// app.post('/logout', 
+// 	req.session.destroy(function (err){
+// 		res.render('login')
+// 	}))
+// ////
 
 
 //Server 
